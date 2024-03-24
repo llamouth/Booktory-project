c149ab3f6b5a26e7f03a166102ccca37310510af
llamouth <150949923+llamouth@users.noreply.github.com>
2024-03-24 07:48:10 -0400

    Added Comments

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+// ! Grabbing the elements needed from html / creating global variables used multiple times in the code
 const [formButton, submitButton] = document.querySelectorAll("button");
 const popUp = document.getElementById("pop-up");
 const form = document.getElementById("form");
-const errorMsg = document.createElement("p");
-const completeMsg = document.createElement("p");
 const bookList = document.getElementById("books")
 const exitButton = document.getElementById("exit")
+const errorMsg = document.createElement("p");
+const completeMsg = document.createElement("p");
 
+// ! Function to remove the hidden class from the pop up to display the popup form
 const openPopUp = () => {
     popUp.classList.remove("hidden")
 }
 
+// ! Function to add the hidden class to the pop up to remove the popup form from display
+const hidePopUp = () => {
+    popUp.classList.add("hidden")
+}
+
+// ! Function to validate the input text
 const validateForm = () => {
     for(let i = 0; i < form.length - 2; i++) {
-        if(form[i].value === ""){
+        if(form[i].value.trim() === ""){
             errorMsg.classList.add("incomplete")
             errorMsg.textContent = "Missing Required Information"
             popUp.append(errorMsg)
@@ -27,8 +35,13 @@ const validateForm = () => {
     return true
 }
 
+const removeBook = (trash, book) => {
+    trash.addEventListener("click", () => {
+        book.remove()
+    })
+}
 
-const stockButtonEvent = (stock) => {
+const changeStateOfStock = (stock) => {
     stock.addEventListener("click", () => {
         if(stock.classList.contains("notStocked")) {
             stock.classList.remove("notStocked")
@@ -43,15 +56,15 @@ const stockButtonEvent = (stock) => {
 }
 
 const createBook = (bookObj) => {
-    
-    
-    const book = document.createElement("li")
-    const imgTag = document.createElement("img")
-    const bookInfo = document.createElement("div")
-    const titleTag = document.createElement("p")
-    const authorTag = document.createElement("p")
-    const priceTag = document.createElement("p")
-    const stockButton = document.createElement("div")
+
+    const book = document.createElement("li");
+    const imgTag = document.createElement("img");
+    const bookInfo = document.createElement("div");
+    const titleTag = document.createElement("p");
+    const authorTag = document.createElement("p");
+    const priceTag = document.createElement("p");
+    const stockButton = document.createElement("div");
+    const trash = document.createElement("i");
 
     book.setAttribute("class", "book");
     imgTag.setAttribute("src", bookObj.img);
@@ -61,21 +74,28 @@ const createBook = (bookObj) => {
     priceTag.setAttribute("class", "price")
     stockButton.setAttribute("class", `${bookObj.stock === "In-Stock" ? "stocked" : "notStocked"}`)
     stockButton.setAttribute("id", "stock")
+    trash.setAttribute("class", "fa-regular fa-trash-can")
 
     titleTag.textContent = bookObj.title;
     authorTag.textContent = bookObj.author
     priceTag.textContent = bookObj.price
     stockButton.textContent = bookObj.stock
 
-    stockButtonEvent(stockButton)
-    bookInfo.append(titleTag, authorTag, stockButton, priceTag)
-    book.append(imgTag, bookInfo)
-    bookList.append(book)
+    bookInfo.append(titleTag, authorTag, stockButton, priceTag, trash);
+    book.append(imgTag, bookInfo);
+    bookList.append(book);
+
+    removeBook(trash, book);
+    changeStateOfStock(stockButton);
 }
 
 
 formButton.addEventListener("click", (e) => {
-    openPopUp()
+    if(popUp.classList.contains("hidden")){
+        openPopUp();
+    }else {
+        hidePopUp();
+    }
     e.preventDefault()
 })
 
@@ -89,6 +109,7 @@ submitButton.addEventListener("click", (e) => {
             price: price.value,
             stock: stock.value,
         }
+
         createBook(bookObj)
         popUp.classList.add("hidden")
 
@@ -101,5 +122,6 @@ submitButton.addEventListener("click", (e) => {
 })
 
 exitButton.addEventListener("click", () => {  
-    popUp.classList.add("hidden")
+    hidePopUp();
+    e.preventDefault();
 })
