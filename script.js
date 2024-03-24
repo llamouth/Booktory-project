31f4ce6224770d96aca946f1668d0fe413b5e86a
llamouth <150949923+llamouth@users.noreply.github.com>
2024-03-24 06:47:36 -0400

    Set-up Personal Booktory

--- script.js
+++ script.js
@@ -1,143 +1,105 @@
-// ! Retrieving the elements needed from the HTML
-const books = document.getElementById("books");
-const addButton = document.getElementById("add");
-const template = document.getElementById("template")
-const layout = document.getElementById("layout")
-const title = document.getElementById("title");
-const asideb = document.getElementById("aside-b");
+const [formButton, submitButton] = document.querySelectorAll("button");
+const popUp = document.getElementById("pop-up");
+const form = document.getElementById("form");
 const errorMsg = document.createElement("p");
-const completionMsg = document.createElement("p");
-const removeMsg = document.createElement("p")
+const completeMsg = document.createElement("p");
+const bookList = document.getElementById("books")
+const exitButton = document.getElementById("exit")
 
-addButton.addEventListener("click", (e) => {
-   
-    const form = e.target.form;
-        
-    if (validateForm(form)) { 
-        const [title, author, image, price, stock] = form;
-        const bookObj = {
-            title: title.value,
-            author: author.value,
-            image: image.value,
-            price: `$${price.value}`,
-            stockValue: stock.value, 
-            inStock: `${stock.value === "In-Stock"}`
-        };
+const openPopUp = () => {
+    popUp.classList.remove("hidden")
+}
 
-        books.append(generateBook(bookObj)); 
-        
-        
-        handleFormCompleteMessage();
-        displayTemplate();
-
-        title.value = "";
-        author.value = "";
-        image.value = "";
-        price.value = "";
-    }else { 
-        handleFormErrorMessage(); 
+const validateForm = () => {
+    for(let i = 0; i < form.length - 2; i++) {
+        if(form[i].value === ""){
+            errorMsg.classList.add("incomplete")
+            errorMsg.textContent = "Missing Required Information"
+            popUp.append(errorMsg)
+            completeMsg.remove()
+            return false
+        }
     }
+    completeMsg.classList.add("complete")
+    completeMsg.textContent = "Book Added"
+    bookList.append(completeMsg)
+    errorMsg.remove()
+    return true
+}
 
-    e.preventDefault();
+
+const stockButtonEvent = (stock) => {
+    stock.addEventListener("click", () => {
+        if(stock.classList.contains("notStocked")) {
+            stock.classList.remove("notStocked")
+            stock.classList.add("stocked")
+            stock.textContent = "In-Stock"
+        }else {
+            stock.classList.remove("stocked")
+            stock.classList.add("notStocked")
+            stock.textContent = "Not-In-Stock"
+        }
+    })
+}
+
+const createBook = (bookObj) => {
+    
+    
+    const book = document.createElement("li")
+    const imgTag = document.createElement("img")
+    const bookInfo = document.createElement("div")
+    const titleTag = document.createElement("p")
+    const authorTag = document.createElement("p")
+    const priceTag = document.createElement("p")
+    const stockButton = document.createElement("div")
+
+    book.setAttribute("class", "book");
+    imgTag.setAttribute("src", bookObj.img);
+    bookInfo.setAttribute("class", "bookList__info");
+    titleTag.setAttribute("class", "title");
+    authorTag.setAttribute("class", "author");
+    priceTag.setAttribute("class", "price")
+    stockButton.setAttribute("class", `${bookObj.stock === "In-Stock" ? "stocked" : "notStocked"}`)
+    stockButton.setAttribute("id", "stock")
+
+    titleTag.textContent = bookObj.title;
+    authorTag.textContent = bookObj.author
+    priceTag.textContent = bookObj.price
+    stockButton.textContent = bookObj.stock
+
+    stockButtonEvent(stockButton)
+    bookInfo.append(titleTag, authorTag, stockButton, priceTag)
+    book.append(imgTag, bookInfo)
+    bookList.append(book)
+}
+
+
+formButton.addEventListener("click", (e) => {
+    openPopUp()
+    e.preventDefault()
 })
 
-const validateForm = (formArr) => {
-
-    for (let i = 0; i < formArr.length - 1; i++) {
-        if (formArr[i].value === "") {
-            return false;
+submitButton.addEventListener("click", (e) => {
+    if(validateForm()){
+        const [title, author, img, price, stock] = form
+        const bookObj = {
+            title: title.value, 
+            author: author.value,
+            img: img.value,
+            price: price.value,
+            stock: stock.value,
         }
+        createBook(bookObj)
+        popUp.classList.add("hidden")
+
+        form[0].value = ""
+        form[1].value = ""
+        form[2].value = ""
+        form[3].value = ""
     }
-    return true;
-}
+    e.preventDefault()
+})
 
-const handleFormErrorMessage = () => {
-
-    errorMsg.textContent = "Missing Required Book Information. Please fill out all fields";
-    errorMsg.setAttribute("class", "form__incomplete");
-    asideb.append(errorMsg);  
-    completionMsg.remove();    
-}
-
-const handleFormCompleteMessage = () => {
-
-    completionMsg.textContent = "Book Added"
-    completionMsg.setAttribute("class", "form__complete");
-    asideb.append(completionMsg)
-    errorMsg.remove()
-}
-
-const displayTemplate = () => {
-
-    const bookList = books.getElementsByTagName("li")
-    if(bookList.length === 0) {
-        books.append(template)
-    }else {
-        template.remove()
-    }
-}
-
-const generateBook = (bookObj) => {
-
-    const book = document.createElement("li");
-    const bookInfo = document.createElement("div");
-    const img = document.createElement("img");
-    const title = document.createElement("p");
-    const author = document.createElement("p");
-    const stock = document.createElement("div");
-    const price = document.createElement("p");
-    const trash = document.createElement("i");
-
-    book.setAttribute("class", "books__book")
-    bookInfo.setAttribute("class", "books__book-info")
-    img.setAttribute("src", bookObj.image)
-    title.setAttribute("class", "title")
-    author.setAttribute("class","author")
-    stock.setAttribute("class", `${bookObj.stockValue === "In-Stock" ? "stocked" : "notStocked"}`)
-    price.setAttribute("class", "price")
-    trash.setAttribute("class", "fa-solid fa-trash-can")
-    
-    title.textContent = bookObj.title
-    author.textContent = bookObj.author
-    stock.textContent = bookObj.stockValue
-    price.textContent = bookObj.price
-    
-    bookInfo.append(title, author, stock,  price, trash)
-    book.append(img, bookInfo)
-
-    addEventsToBook([trash, book], stock)
-
-    return book
-}
-
-
-
-const addEventsToBook = (trashButtonArray, stockButton) => {
-
-    trashButtonArray[0].addEventListener("click", () => {
-        trashButtonArray[1].remove()
-        removeMsg.setAttribute("class", "remove");
-        removeMsg.textContent = "Book Removed"
-        
-        displayTemplate()
-    })
-
-    stockButton.addEventListener("click", () => {
-        if(stockButton.classList.contains("notStocked")) {
-            stockButton.classList.remove("notStocked")
-            stockButton.classList.add("stocked")
-            stockButton.textContent = "In-Stock"
-        }else {
-            stockButton.classList.remove("stocked")
-            stockButton.classList.add("notStocked")
-            stockButton.textContent = "Not-In-Stock"
-        }
-    })
-}
-
-// * Update BEM to appropriate naming, focus on component-based naming
-// * Update your JS to incorporate the updated BEM names
-// * create functions for code that is too long inside of other functions
-// * create a completion element and create a separate errormessage element
-
- 
+exitButton.addEventListener("click", () => {  
+    popUp.classList.add("hidden")
+})
